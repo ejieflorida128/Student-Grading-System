4023e70e20297634d6585d3e868c745aae02bb2c
Athena Joy Campania <151619683+AthenaJoy@users.noreply.github.com>
2023-12-23 22:43:36 +0800

    [athena] kani sa langga ka sleepon nako haha

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grading System</title>
+</head>
+<body>
+    <form>
+        <div class="portal">
+         <h1> Login </h1>
+                <div class="portal_box">
+                    <input type="email" placeholder="Email" name="Email" required>
+                    </div>
+                    <div class="portal_box">
+                        <input type="password" placeholder="Password" name="Password" required>
+                        </div>
+                        <div class="btn">
+                            <button type="submit" >Log in </button>
+                        </div>
+
+                        <div class= "Register">
+                            <p> Don't have an account? <a href="register">Register here</a></p>
+
+                            </div>
+        </div>
+    </form>
+</body>
+</html>
